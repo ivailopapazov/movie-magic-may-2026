fe554243be08db89f289ee17d5aa62fb60a9c0c3
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2026-07-01 20:56:29 +0300

    Show dynamic list of artists on attach page

--- src/repositories/artistRepository.js
+++ src/repositories/artistRepository.js
@@ -1,14 +1,21 @@
 import { prisma } from '../lib/prisma.js';
 
+export async function getAll() {
+    const artists = await prisma.artist.findMany();
+
+    return artists;
+}
+
 export async function create(artistData) {
     const artist = await prisma.artist.create({
         data: artistData,
     });
-    
+
     return artist;
 }
 
 const artistRepository = {
+    getAll,
     create,
 };
 
