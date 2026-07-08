06811ac1cfc5eee6c887b293d0892ae8cf4bd572
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2026-07-08 19:31:16 +0300

    Add auth middleware

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { engine } from 'express-handlebars';
 
 import routes from './routes.js';
+import { authMiddleware } from './middlewares/authMiddleware.js';
 
 const app = express();
 
@@ -17,6 +18,9 @@ app.use(express.static('./src/public'));
 
 // Setup body parser
 app.use(express.urlencoded());
+
+// Auth middleware
+app.use(authMiddleware);
 
 // Setup routes
 app.use(routes);
